a31e5a1126c5bcb8c072796d3ec66c4b149cb668
relyonm <relyon.master@gmail.com>
2025-09-02 15:45:28 +0700

    feat: generate project using spring initializr

---
+++ src/test/java/com/grab/test/load/GrabLoadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grab.test.load;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabLoadTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
