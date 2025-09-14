77459251ee434ccda24c18f8bd52d81e74326247
Man Nguyen Minh <mannm.engineer@gmail.com>
2025-09-14 15:49:39 +0700

    wip

--- src/main/java/com/grab/test/load/GrabServiceSimulation.java
+++ src/main/java/com/grab/test/load/GrabServiceSimulation.java
@@ -4,7 +4,6 @@ import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.StringBody;
 import static io.gatling.javaapi.core.CoreDsl.rampUsers;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
@@ -14,21 +13,19 @@ public class GrabServiceSimulation extends Simulation {
 
   // Define HTTP protocol (adjust base URL to your Spring Boot app)
   private HttpProtocolBuilder httpProtocol = http
-    .baseUrl("http://34.124.188.225:30002/api") // adjust if you use context-path
-    .contentTypeHeader("application/json");
+    .baseUrl("http://localhost:8080");
 
-  // Scenario for creating driver
-  private ScenarioBuilder createDriverScenarioBuilder = scenario("Load Test: Create Driver")
+  // Scenario for getting drivers
+  private ScenarioBuilder getDriversScenarioBuilder = scenario("Load Test: Get Drivers")
     .exec(
-      http("Create Driver")
-        .post("/drivers")
-        .body(StringBody("{\"fcmToken\": \"sample-token-123\"}"))
-        .check(status().is(201)) // expect CREATED
+      http("Get Drivers")
+        .get("/drivers")
+        .check(status().is(200)) // expect OK
     );
 
   {
     setUp(
-      createDriverScenarioBuilder.injectOpen(
+      getDriversScenarioBuilder.injectOpen(
         rampUsers(60000).during(60) // 10 users over 10 seconds
       )).protocols(httpProtocol);
   }
